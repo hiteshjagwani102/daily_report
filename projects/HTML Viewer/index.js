25d6205600d0eec5d8af5e8771837bc8f8c729d4
Hitesh Jagwani <60232373+hiteshjagwani102@users.noreply.github.com>
2023-02-22 18:02:57 +0530

    imporved UI of HTML Viewer

--- projects/HTML Viewer/index.js
+++ projects/HTML Viewer/index.js
@@ -1,3 +1,4 @@
+// input_config
 var input = ace.edit("input");
 input.setOptions({
     placeholder: "Type or copy you code here...",
@@ -8,7 +9,7 @@ document.getElementById('input').style.fontSize=`${size1}px`;
 input.session.setMode("ace/mode/html");
 
 
-
+// output_config
 var output = ace.edit("output");
 output.setOptions({
     placeholder: "Type or copy you code here...",
@@ -18,3 +19,18 @@ output.setTheme("ace/theme/ambiance")
 var size2 = 15;
 document.getElementById('output').style.fontSize=`${size2}px`;
 output.session.setMode("ace/mode/html");
+
+
+// upload
+function openCode(files) {
+    var file = files[0]
+    if (!file) return;
+    var modelist = ace.require("ace/ext/modelist")
+    var modeName = modelist.getModeForPath(file.name).mode 
+    input.session.setMode(modeName)
+    reader = new FileReader();
+    reader.onload = function() {
+        input.session.setValue(reader.result)
+    }  
+    reader.readAsText(file) 
+}
