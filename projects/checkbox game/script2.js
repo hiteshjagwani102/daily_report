bb617f15f99f8295c260e0082f9d1e25426b0033
Hitesh Jagwani <60232373+hiteshjagwani102@users.noreply.github.com>
2023-05-04 19:43:10 +0530

    Add level 2

---
+++ projects/checkbox game/script2.js
@@ -0,0 +1,46 @@
+var first = document.getElementById('first');
+var width = window.innerWidth;
+var height = window.innerHeight;
+first.style.marginTop =`${height/2}px`
+
+
+var val = 0;
+var score = 0;
+
+const doOnClick =(e) =>{
+    
+
+}
+
+
+
+document.querySelectorAll('.check').forEach((el)=>{
+    el.style.transition = `transform 0.2s ease`;
+    el.style.marginLeft = `100px`;
+    el.style.height='fit-content'
+    el.style.marginRight = `100px`
+    el.style.marginBottom= `5px`
+    
+    el.addEventListener('click',()=>{
+        if(el.checked){
+            score++;
+            val+=22;
+            first.style.marginTop = `${height/2-val}px`;
+            
+            var sign = Math.floor(Math.random()*10);
+            if(sign%2==0) el.nextElementSibling.style.transform = `translateX(${-Math.random()*50}px)`;
+            else el.nextElementSibling.style.transform = `translateX(${+Math.random()*50}px)`;
+
+            
+        }
+    else{
+        score--;
+        first.style.marginLeft = `${width/2-val+22}px`;
+        val-=22;
+    }
+    document.getElementById('score').innerText= `Current Score: ${score}`;
+        
+    })
+    // el.addEventListener('click',doOnClick);
+    
+})
