8af43f28ef7e2c3897d10ffbbfccadf29ab74859
Hitesh Jagwani <60232373+hiteshjagwani102@users.noreply.github.com>
2023-06-05 16:29:15 +0530

    add link access

--- projects/video_loop/script.js
+++ projects/video_loop/script.js
@@ -5,7 +5,6 @@ var endTime = 0;
 var started = false;
 var duration = 0;
 
-const linkInput = document.getElementById('link-input');
 const linkList = document.getElementById('link-list');
 const popular = document.getElementById('popular');
 
@@ -53,8 +52,8 @@ function displayLinks(links) {
   const handleClick = (e) => {
     e.preventDefault();
     const clickedLink = e.target.getAttribute('data-link');
-    videoLink = clickedLink;
-    embedVideo2();
+    document.getElementById('video-link').value = clickedLink;
+    embedVideo();
   };
   
   linkList.addEventListener('click', handleClick);
@@ -63,52 +62,6 @@ function displayLinks(links) {
   let storedLinks = localStorage.getItem('youtubeLinks');
   storedLinks = storedLinks ? JSON.parse(storedLinks) : [];
   displayLinks(storedLinks);
-
-async function embedVideo2(){
-    var videoId = extractVideoId(videoLink);
-
-    var embedUrl = "https://www.youtube.com/embed/" + videoId + "?enablejsapi=1&start=" + startTime + "&end=" + endTime + "&loop=1" + "&autoplay=1" + "&playlist="+videoId;
-  var playerContainer = document.getElementById('player-container');
-  playerContainer.innerHTML = '<iframe id="player" width="560" height="315" style="border-radius:10px;" src="' + embedUrl + '" frameborder="0";encrypted-media; gyroscope;  allowfullscreen></iframe>';
-  document.getElementById('holder').style.display = "none";
-
-  var playerIframe = document.getElementById('player');
-  player = new YT.Player(playerIframe, {
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-
-    const link = videoLink;
-
-    if (link.trim() !== '') {
-
-      var head = "";
-      await getTitle(videoId).then(title => {
-           head = title
-        })
-
-      const videoDetails = {
-        link: link,
-        thumbnail: getThumbnailUrl(videoId),
-        title: head
-      };
-
-      let links = localStorage.getItem('youtubeLinks');
-      links = links ? JSON.parse(links) : [];
-
-      links.unshift(videoDetails);
-
-      if (links.length > 3) {
-        links = links.slice(0, 3);
-      }
-
-      localStorage.setItem('youtubeLinks', JSON.stringify(links));
-      displayLinks(links);
-    }
-}
-
 
 async function embedVideo() {
   var videoLink = document.getElementById('video-link').value;
@@ -236,10 +189,11 @@ document.getElementById('slider-2').addEventListener('change',()=>{
 
 setInterval(check,100);
 function check() {
-  if(started){
+  if(started && getCurrentTime){
   if(endTime<=player.getCurrentTime()){
     player.seekTo(sliderOne.value/1000*player.getDuration());
   }
+  else check();
 }
 }
 
@@ -292,13 +246,46 @@ document.getElementById('restart').addEventListener('click',()=>{
 })
 
 
-const openSidebar = () => {
-  document.getElementById("mySidebar").style.width = "40%";
-};
+var auto_embed = document.getElementById('auto');
 
-const closeSidebar = () => {
-  document.getElementById("mySidebar").style.width = "0";
-};
+function validLink(videoLink){
+  var match = videoLink.match(/youtube\.com.*(\?v=|\/embed\/|\/\d\/|\/vi\/|v\/|\/u\/\w\/|\/embed\/|embed\/)([^#\&\?]*).*/i);
+  if (match && match[2].length == 11) {
+    return true;
+  } else return false;
+}
+
+document.getElementById('video-link').addEventListener('keyup', () =>{
+    if(auto_embed.checked && validLink(document.getElementById('video-link').value)){
+      console.log("yes");
+      embedVideo();
+    }
+})
+
+var sample = document.getElementById('sample').addEventListener('click',()=>{
+  document.getElementById('video-link').value = "https://www.youtube.com/watch?v=UwsrzCVZAb8";
+  embedVideo();
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+  getLinkData();
+
+}, false);
+
+function getLinkData(){
+  var url = window.location.href;
+  var urlParams = new URLSearchParams(url);
+
+  if(url.includes('?')){
+    for (let pair of urlParams.entries()) {
+      document.getElementById("video-link").value = pair[1];
+      embedVideo()
+   }
+   
+      
+  }
+}
+
 
 
 
